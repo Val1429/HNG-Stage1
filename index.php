6fb6871d9cfbe7c3dd57b352dfc92182fcc89a4e
Val1429 <38227610+Val1429@users.noreply.github.com>
2018-04-13 06:13:00 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<link rel="stylesheet" href="style.css" >
+	<title>HNG INTERNSHIP 4.0</title>
+</head>
+
+<body>
+<div id="te">
+ <center>HNG INTERNSHIP 4.0 </center>
+</div>
+<div id="ti">
+<center>
+<?php 
+	date_default_timezone_set('Africa/Lagos');
+	echo date("h:i:sa"); 
+	?>
+</center>
+</div> 
+</body>
+</html> 
